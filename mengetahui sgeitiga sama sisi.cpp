fcf8a8d2a252f1ffe57461f93a4d0bea2cfe2154
wanda belingga putra <45660041+WandaBelinggaPutra@users.noreply.github.com>
2019-01-04 20:55:30 +0700

    Add files via upload

---
+++ mengetahui sgeitiga sama sisi.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	
+int a,b,c;
+
+cout<<"menentukan segitiga sama sisi"<<endl;
+cout<<"masukan panjang sisi a"<<endl;
+cin>>a;
+cout<<"masukan panjang sisi b"<<endl;
+cin>>b;
+cout<<"masukan panjang sisi c"<<endl;
+cin>>c;
+
+if(a==b && a==c)
+	cout<<"segitiga tersebut sama sisi:";
+	else
+		cout<<"segitiga tersebut tidak sama sisi:";
+	return 0;
+	
+}
